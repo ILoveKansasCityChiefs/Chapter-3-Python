7d27a0b04a39c207068ebb717670e9e40687ef67
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-05 14:30:11 -0400

    Add pizza lists for self and friends
    Create a pizza list and display favorites for self and friends.

---
+++ pizzas4-11.py
@@ -0,0 +1,11 @@
+pizzas = ['Cheese', 'Pepperoni', 'Sausage']
+friends_pizzas = pizzas[:]
+
+pizzas.append('Veggie')
+friends_pizzas.append('Mushroom')
+
+print("My favorite pizzas are:")
+print(pizzas)
+
+print("\nMy friends favorite pizzas are:")
+print(friends_pizzas)
