3db6b9ea7fcd062156284373a164a6a37e232665
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-05 14:24:21 -0400

    Create friends4-10.py

---
+++ friends4-10.py
@@ -0,0 +1,10 @@
+friends = ['Raymor', 'Meekdad', 'Michael', 'Joshka', 'Lucas', 'Skylar']
+
+print("The first three friends in the list are: ")
+print(friends[0:3])
+
+print("\nTwo friends in the middle of the list are: ")
+print(friends[3:4])
+
+print("\nOne friend at the end of the list is: ")
+print(friends[5:])
