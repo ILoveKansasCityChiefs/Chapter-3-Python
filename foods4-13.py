fbdedc9ba318d880a079e5b60e51d2252116c2c5
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-05 14:44:41 -0400

    Add 'Fufu' and 'Fish' to buffet_foods
    Updated buffet_foods to include 'Fufu' and 'Fish'.

---
+++ foods4-13.py
@@ -0,0 +1,7 @@
+buffet_foods = ("CrabLegs", "Macaroni", "PIzza", "Dessert", "Cheese")
+for food in buffet_foods:
+    print(food)
+
+buffet_foods = ("CrabLegs", "Macaroni", "PIzza", "Dessert", "Cheese", "Fufu", "Fish")
+for food in buffet_foods:
+    print(food)
