e68fa4711605e3fffea0d7c52068bfbeb713e7a4
ILoveKansasCityChiefs <shawndelingoa@gmail.com>
2026-07-05 13:32:59 -0400

    Add corvids list and print statements

---
+++ animals4-2.py
@@ -0,0 +1,6 @@
+corvids = ['Raven', 'Jay', 'Nutcracker']
+for corvid in corvids:
+    print(corvid)
+    print(f"A {corvid.title()} is a corvid! \n")
+    
+print("All of these bird speices are corvids!")
